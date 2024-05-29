f5b46e8f62fe31b97f88673e63665aa5427d8f4d
Your Name <you@example.com>
2024-05-29 19:43:14 +0100

    homework food and recipe

---
+++ park_count_homework.py
@@ -0,0 +1,18 @@
+trip_from_and_back = (600 * 3 + 500) * 4
+pizza_cost = 250 * 2 - (2 * 250 * 0.15)
+air_hochey = 8 * 80
+divide_by_amount_of_people = (trip_from_and_back + pizza_cost + air_hochey) / 4
+writing_result = f"Every friend need to pay {divide_by_amount_of_people} гривн"
+print(writing_result)
+
+
+
+
+
+
+
+
+
+
+
+
