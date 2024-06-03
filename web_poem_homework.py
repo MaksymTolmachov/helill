5a31c7f49e86e5ca3a10c051379debd3a7340908
Your Name <you@example.com>
2024-06-03 16:14:05 +0100

    homework food and recipe

---
+++ web_poem_homework.py
@@ -0,0 +1,22 @@
+from pywebio.input import input as input_pw
+from pywebio.output import put_html, put_text, put_image
+
+
+#header
+put_html("<h1>Its finally summer🎉!!!</h1>")
+put_html("<h2> Сонце гріє </2h> ")
+summer_poem = """Сонце гріє, вітер віє
+З поля на долину,
+Над водою гне з вербою
+Червону калину;
+На калині одиноке
+Гніздечко гойдає, -
+А де ж дівся соловейко?
+Не питай, не знає. """
+put_text(summer_poem)
+question_what_plans_for_your_summer = input_pw("What plans do you have for this summer?")
+
+symbols_in_my_summer_plans = len(question_what_plans_for_your_summer)
+put_text(f"{symbols_in_my_summer_plans} symbols was used in your writing.")
+img = open("Summer-1170x650-1.jpg", "rb").read()
+put_image(img, width="650px")
