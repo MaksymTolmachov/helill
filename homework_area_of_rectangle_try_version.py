099f6cce35331b65d144928603dd6072c068d2dc
Your Name <you@example.com>
2024-06-06 15:44:35 +0100

    homework food and recipe

---
+++ homework_area_of_rectangle_try_version.py
@@ -0,0 +1,26 @@
+
+from pywebio.output import put_html, put_text
+from pywebio.input import input as input_pw
+
+
+user_surname = input_pw("Enter your surname")
+
+
+
+
+#I don't really understand the task.
+
+length_side_1 = input_pw("Enter first side of your rectangle")
+length_side_2 = input_pw("Enter second side of your rectangle")
+
+def get_area_of_rectangle(side_number_1, side_number_2):
+    area_of_rectangle = side_number_1 * side_number_2
+    return area_of_rectangle
+
+
+def get_area_of_rectangle(side_number_1, side_number_2):
+    area_of_rectangle = side_number_1 * side_number_2
+    return area_of_rectangle
+
+
+get_area_of_rectangle(3, 5)
