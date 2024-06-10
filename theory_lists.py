d900e42483f45af7f7b31f12ac8955e036d4bc82
Your Name <you@example.com>
2024-06-10 19:05:16 +0100

    homework food and recipe

---
+++ theory_lists.py
@@ -0,0 +1,63 @@
+# cities = "Lviv Odesa Kherson".split()
+# print(cities[1])
+# odesa_name = cities[1].upper
+# cities[1] = "Kyiv"
+# cities.append(1111)
+# cities.append(True)
+# cities.append(None)
+#
+# print(len(cities))
+new_buyings = [
+
+]
+
+mom_buyings = [
+    "bread",
+    "milk",
+    "ice-cream",
+]
+
+stringss = "gggh" + "jjjk"
+
+sister_wish = "doll"
+mom_buyings.append(sister_wish)
+
+father_wish = [
+    "beer",
+    "chips",
+]
+
+#mom_buyings.append(father_wish)
+mom_buyings.extend(father_wish)
+
+mom_buyings.remove("beer")
+mom_buyings.pop(0)
+mom_buyings.insert(0,"matches")
+final_list_buing = mom_buyings * 3
+
+
+
+number = 10
+number += 4
+number /= 2
+number *= 1
+
+mom_buyings += father_wish
+
+for product in final_list_buing:
+    print(     product)
+
+
+
+
+pass
+
+
+
+
+
+
+
+
+
+
