182650adbacd52f11a8098c026c177d6863106fd
Your Name <you@example.com>
2024-07-04 17:00:39 +0100

    homework food and recipe

---
+++ main.py
@@ -0,0 +1,19 @@
+import email_sender_homework
+
+def main():
+    list_of_recipients = [
+        "aaaa@ukr.net",
+
+    ]
+
+    # email_sender_homework.send_email(
+    #     list_of_recipients, mail_subject="Head", mail_body="Body"
+    # )
+
+    email_sender_homework.create_welcome_letter({"user": "Alex"})
+
+if __name__ == '__main__':
+    main()
+
+
+
