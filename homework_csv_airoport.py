8a2f992a788695df5fcecefe8e4a49bf2f59ffe3
Your Name <you@example.com>
2024-07-11 15:52:20 +0100

    homework food and recipe

---
+++ homework_csv_airoport.py
@@ -0,0 +1,11 @@
+import csv
+ukranian_airport = "UA"
+with open("airport-codes_csv.csv", mode="r",) as file:
+    reader = csv.DictReader(file, delimiter=";")
+    for row in reader:
+        if reader:
+            row["iso_country"] = ukranian_airport
+            total = row
+            print(total)
+
+
