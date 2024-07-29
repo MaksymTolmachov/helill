500a7b73cbf52c5ae7a5125bd134e3c1e5068c6f
Your Name <you@example.com>
2024-07-29 19:20:21 +0100

    homework food and recipe

--- homework_dict.py
+++ homework_dict.py
@@ -34,39 +34,33 @@ students = {
     },
 }
 # ваш код нижче !!!!!!!! вище нічого не змінюємо
-from pywebio.input import input_group,slider, textarea, select, checkbox, radio, FLOAT, DATE, PASSWORD, DATETIME, input as input_pw
-from pywebio.output import put_success, put_error, put_warning, put_html,put_table, put_image
-from pywebio.session import run_js
-from pywebio import start_server
-from pprint import pprint
+
 
 students["Alex Kolesnuk"] = {
     'Пошта': 'Alex.Kolesnuk2005@gmail.com',
     'Вік': 19,
     'Номер телефону': '+380096347829',
-    'Середній бал': 74
+    'Середній бал': 90
 }
 
 lowest_scour = 90
+total_coumlate_score = 0
 for student_name, data in students.items():
     if data["Середній бал"] >= lowest_scour :
         print(f"{student_name}, {data['Середній бал']}, {data['Вік']} years old")
 
-
-def get_average_score(score_1: int, score_2: int,  score_3: int,  score_4: int):
-    score_4 = 95.8
-    score_2 = 64.5
-    score_3 = 80
-    score_1 = 74
-    altogether = score_2 + score_1 + score_3 + score_4
-    result = altogether / 4
-    return result
+    all_scores_altogether = total_coumlate_score =+ data["Середній бал"]
+    result = all_scores_altogether / len(students)
 
 
-for student_name, data in students.items():
-    if data['Номер телефону'] == None:
+
+    if data['Номер телефону'] is None:
         data['Номер телефону'] = "+380096875201"
         print(data['Номер телефону'])
+print(total_coumlate_score)
+
+
+
 
 
 
